0bd233f1ab02da2ccb0d667a97bd1f6e2dbac812
Ignat Anashin <firebat75@yandex.ru>
2020-04-29 19:43:56 +0300

    controllers created

---
+++ controllers/articles.js
@@ -0,0 +1,38 @@
+const Article = require('../models/article');
+const NotFoundError = require('../errors/NotFoundError');
+const Forbidden = require('../errors/Forbidden');
+const ReqError = require('../errors/ReqError');
+
+module.exports.getArticles = (req, res, next) => {
+  Article.find({})
+    .populate('owner')
+    .then((articles) => {
+      if (!articles) {
+        throw new NotFoundError('No articles were found');
+      }
+      res.status(200).send({ data: articles });
+    })
+    .catch(next);
+};
+
+module.exports.postArticle = (req, res, next) => {
+  const { keyword, title, text, date, source, link, image } = req.body;
+  Article.create({ keyword, title, text, date, source, link, image })
+    .then((article) => res.status(200).send({ data: article }))
+    .catch(next);
+};
+
+module.exports.removeArticle = (req, res, next) => {
+  Article.findById(req.params.articleId)
+    .then((article) => {
+      if (!article) {
+        throw new ReqError('no article with this id');
+      }
+      if (!article.owner.equals(req.user._id)) {
+        throw new Forbidden('you can delete only yours articles');
+      }
+      Article.findByIdAndDelete(req.params.cardId)
+        .then((articles) => res.status(200).send({ data: articles }));
+    })
+    .catch(next);
+};
---
+++ controllers/users.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const NotFoundError = require('../errors/NotFoundError');
+const Unauthorized = require('../errors/Unauthorized');
+
+
+module.exports.getUser = (req, res, next) => {
+  User.find({})
+    .then((me) => {
+      if (!me) {
+        throw new NotFoundError('User identification error');
+      }
+      res.status(200).send({ data: me });
+    })
+    .catch(next);
+};
+
+//не менял дальше
+
+module.exports.addUser = (req, res, next) => {
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  const unauthorized = new Unauthorized('');
+
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, about, email, password: hash, avatar,
+    }))
+    .then((user) => res.send({
+      name: user.name, about: user.about, email: user.email, avatar: user.avatar,
+    }))
+    .catch((err) => {
+      if (err.code === 11000 && err.name === 'MongoError') {
+        res.status(unauthorized.statusCode).send({ message: 'Email already registred' });
+      }
+    })
+    .catch(next);
+};
+
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'top-secret');
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).send();
+    })
+    .catch(next);
+};
