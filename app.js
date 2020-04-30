2d83cfe79b5c5c60b2bc7a1283050ba845474498
Ignat Anashin <firebat75@yandex.ru>
2020-04-30 10:49:21 +0300

    DB path created

--- app.js
+++ app.js
@@ -11,12 +11,10 @@ const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./middlewares/PageNotFound');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_PATH, NODE_ENV } = process.env;
 const app = express();
 
-// Нужно реализовать: В production-режиме адрес базы данных берётся из process.env 5.3
-
-mongoose.connect('mongodb://localhost:27017/newsdb', {
+mongoose.connect(NODE_ENV === 'production' ? DB_PATH : 'mongodb://localhost:27017/newsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
